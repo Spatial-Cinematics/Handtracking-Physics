b388d20434feb7692fec01a0897d44e7e3541911
clayton <claystamper@gmail.com>
2020-01-30 16:44:25 -0600

    added morph shader and controller script

---
+++ Assets/Materials/Morph.shader
@@ -0,0 +1,82 @@
+﻿shader "Morph"
+{
+    Properties
+    {
+        [Toggle] _RecieveLight ("Reiceve Light", Float) = 0
+        _Color ("Color", Color) = (1,1,1,1)
+        _RimColor ("Rim Color", Color) = (.5, .5, 1, 1)
+        _RimPower ("Rim Power", Range (.5, 8)) = 3
+        _Intensity ("Rim Intensity", Range (1, 5)) = 3
+        _Warp ("Affector Strength", Range (0, 1)) = 1
+        _Affector ("_Affector", vector) = (0,0,0)
+        [Toggle] _Reverse ("Push", float) = 0
+    }
+    SubShader
+    {
+
+        Tags { "Queue" = "Transparent"}
+
+        CGPROGRAM
+        #pragma surface surf NoLighting vertex:vert alpha:fade
+
+        float _RecieveLight;
+
+        half4 LightingNoLighting (SurfaceOutput s, half3 lightDir, half atten){
+            half NdotL = dot(s.Normal, lightDir); //normal light value
+            half4 c;
+            c.rgb = s.Albedo;
+            c.rbg *= _RecieveLight == 0 ? 1 : (NdotL * atten);
+            c.a = s.Alpha;
+            return c;
+        }
+
+        float4 _Color;
+        float4 _RimColor;
+        float _RimPower;
+        half _Intensity;
+
+        struct Input
+        {
+            float3 viewDir;
+            float3 worldPos;
+        };
+        struct appdata {
+            float4 vertex : POSITION;
+            float3 normal : NORMAL;
+            float4 TEXCOORD : TEXCOORD0;
+        };
+
+        int _NodesLength;
+        float _Trans;
+        float _Warp;
+        float3 _Affector;
+        fixed _Reverse;
+
+        void vert (inout appdata i){
+
+            float3 worldPos = mul (unity_ObjectToWorld, i.vertex);
+            float strength = _Warp - distance(worldPos, _Affector);
+            float3 dir = //_//Reverse == 0 ? 
+                //(normalize(_Affector - worldPos)) :
+                (normalize(worldPos - _Affector));
+
+            float3 warp = strength * dir;
+            //warp *= (dot(dir, i.normal) + 1);
+            i.vertex.xyz += mul (warp, unity_ObjectToWorld).xyz;
+            
+        }
+
+        void surf (Input IN, inout SurfaceOutput o)
+        {
+
+            half rim = 1 - saturate(dot(normalize(IN.viewDir), o.Normal));
+
+            o.Albedo = _Color.rgb;
+            o.Emission = _RimColor.rgb * pow (rim, _RimPower) * _Intensity;
+            o.Alpha = _Color.a;
+            
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
---
+++ Assets/Materials/MorphController.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class MorphController : MonoBehaviour {
+
+    private Vector4[] nodes;
+    private Material mat;
+
+    public GameObject[] affectors;
+    public Transform affector;
+    
+    [Range(0,100)]
+    public float pos;
+    [Range(0,360)]
+    public float rot;
+
+    private int index;
+    private float transition;
+    private Vector3 current, next, nextnext;
+
+    [ContextMenu("Configure")]
+    private void Start() {
+        affectors = GameObject.FindGameObjectsWithTag("Affector");
+        wire.MapSpline();
+        nodes = wire.nodes.ToArray();
+        mat = GetComponent<Renderer>().sharedMaterial;
+        mat.SetInt("_NodesLength", nodes.Length);
+        mat.SetVectorArray("_Nodes", nodes);
+        MoveToPos();
+    }
+    
+    private void Update() {
+
+        if (affectors.Length > 0)
+            affector = GetClosestAffector();
+        
+        if (affector) {
+            mat.SetVector("_Affector", affector.position);
+            mat.SetFloat("_Warp", affector.localScale.x * 1.2f );
+        }
+
+        MoveToPos();
+    }
+
+    public void MoveCatheter(string data) {
+        
+        if (float.TryParse(data, out pos)) {
+            pos = Mathf.Clamp(pos, 0, 100);
+            Debug.Log("Setting Catheter Postion to: " + pos + "%");
+        }
+        else {
+            Debug.LogError("Error: Data passed into MoveCath(f) is in the wrong format");
+        }
+        
+        MoveToPos();
+        
+    }
+
+    [ContextMenu("Move to pos")]
+    public void MoveToPos() {
+
+        if (nodes == null)
+            nodes = wire.nodes.ToArray();
+        
+        SetTransition();
+        transform.position = GetNewPos();
+        transform.rotation = GetNewRot();
+
+    }
+
+    private void SetTransition() {
+        float rawIndex = pos * (nodes.Length - 1) / 100;
+        index = Mathf.RoundToInt(rawIndex);
+        transition = rawIndex - index + .5f;
+        //print("Raw index = " + rawIndex + ", so transition = " + transition);
+    }
+    
+    private Vector3 GetNewPos() {
+        current = nodes[index];
+        next = nodes[Mathf.Clamp(index + 1, 0, nodes.Length - 1)]; 
+        nextnext = nodes[Mathf.Clamp(index + 2, 0, nodes.Length - 1)]; 
+        return Vector3.Lerp(current, next, transition);
+    }
+
+    private Quaternion GetNewRot() {
+        Vector3 currentLook = (next - current).normalized;
+        Vector3 nextLook = (nextnext - next).normalized;
+        Vector3 lookDir = Vector3.Lerp(currentLook, nextLook, transition);
+        Quaternion quatRot = Quaternion.LookRotation(lookDir, Vector3.up);
+        Vector3 newRot = quatRot.eulerAngles;
+        newRot.z = rot;
+        return Quaternion.Euler(newRot);
+    }
+
+    private Transform GetClosestAffector() {
+
+        Transform closest = affectors[0].transform;
+
+        foreach (GameObject i in affectors) {
+            if (i.transform.IsChildOf(transform))
+                continue;
+            if (transform.Distance(i.transform) < transform.Distance(closest)) {
+                closest = i.transform;
+            }
+        }
+
+        return closest;
+
+    }
+    
+}
