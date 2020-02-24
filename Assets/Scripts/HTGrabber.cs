36d87c973196b4d663841848b0acf15040570954
clayton <claystamper@gmail.com>
2020-02-24 17:51:58 -0600

    hand tracking grab script written and untested

--- Assets/Scripts/HTGrabber.cs
+++ Assets/Scripts/HTGrabber.cs
@@ -3,18 +3,90 @@ using System.Collections;
 using System.Collections.Generic;
 using OculusSampleFramework;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HTGrabber : MonoBehaviour {
 
     private OVRHand hand;
-    private HandSkeleton skeleton;
+    private OVRSkeleton skeleton;
 
+    private List<OVRGrabbable> grabbables;
+
+    OVRBone[] fingerTips;
+    private OVRBone palm;
+    
+    private Text debugText;
+    
     private void Start() {
+        
+        debugText = GameObject.Find("DebugText").GetComponent<Text>();
+        hand = GetComponent<OVRHand>();
+        skeleton = GetComponent<OVRSkeleton>();
+        
+        fingerTips = new[] {skeleton.Bones[(int)OVRSkeleton.BoneId.Hand_Pinky3]};
+        palm = skeleton.Bones[0];
 
-        print(FindObjectOfType<OVRSkeleton>());
-        Debug.Break();
     }
 
     private void Update() {
+        CheckGrab();
     }
+
+    private void OnTriggerEnter(Collider other) {
+        
+        OVRGrabbable grabbable = other.GetComponent<OVRGrabbable>();
+        
+        if (grabbable) {
+            grabbables.Add(grabbable);
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other) {
+        
+        OVRGrabbable grabbable = other.GetComponent<OVRGrabbable>();
+        
+        if (grabbable && grabbables.Contains(grabbable)) {
+            grabbables.Add(grabbable);
+        }
+        
+    }
+
+    private OVRGrabbable GetClosestGrabbable() {
+
+        OVRGrabbable closest = grabbables[0];
+
+        foreach (OVRGrabbable grabbable in grabbables) {
+            if (transform.Distance(grabbable.transform) < transform.Distance(closest.transform)) {
+                closest = grabbable;
+            }
+        }
+
+        return closest;
+
+    }
+
+    private void CheckGrab() {
+
+        try {
+            
+            float grabConf = 0;
+
+            foreach (OVRBone fingerTip in fingerTips) {
+                grabConf += fingerTip.Transform.Distance(palm.Transform);
+            }
+
+            debugText.text = $"Closest grabbable is: {GetClosestGrabbable().name} and has a grab confidence of: {grabConf}";
+
+        }
+        catch (Exception e) {
+
+            debugText.text = $"ERROR: palm={palm}\tskeleton={skeleton.name}\tclosest={GetClosestGrabbable()}";
+
+        }
+        
+        
+
+    }
+
 }
