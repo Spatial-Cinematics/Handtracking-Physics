99cfe26b47cecc73a7afafc7c9decd747086b07f
Clayton Stamper <claystamper@gmail.com>
2020-04-19 21:19:15 -0500

    Hand is working well but could use more ik constraints.... next cuprate for physics interactions is mesh scale

--- Assets/Scripts/PhysicsHand.cs
+++ Assets/Scripts/PhysicsHand.cs
@@ -12,14 +12,21 @@ public class FingerMap : IMap {
 
     public Transform inputTransform;
     public Transform ikTarget;
+    public Transform ikHint;
     [Range(0, 0.03f)]
     public float trackingPositionOffset;
     public Vector3 trackingRotationOffset;
+//    public Vector3 hintOffset;
+
+//    public void Init() {
+//        hintOffset = ikTarget.position - ikTarget.TransformPoint(ikHint.position);
+//    }
     
     public void Map() {
         //this offset just "changes the length of the finger" since the collider doesn't line up with the tip
         ikTarget.position = inputTransform.position + inputTransform.right * trackingPositionOffset;
         ikTarget.rotation = inputTransform.rotation * Quaternion.Euler(trackingRotationOffset);
+//        ikHint.position = ikTarget.position + hintOffset;
     }
 
 }
@@ -29,10 +36,16 @@ public class WristMap : IMap{
     
     public Transform inputTransform;
     public Transform ikTarget;
+    [Range(-.2f, 0.2f)]
+    public float posOffsetX, posOffsetY, posOffsetZ;
     public Vector3 trackingPositionOffset;
+    [Range(-90, 90f)]
+    public float rotOffsetX, rotOffsetY, rotOffsetZ;
     public Vector3 trackingRotationOffset;
     public void Map() {
-        ikTarget.position = inputTransform.position + trackingPositionOffset;
+        trackingPositionOffset = new Vector3(posOffsetX, posOffsetY, posOffsetZ);
+        trackingRotationOffset = new Vector3(rotOffsetX, rotOffsetY, rotOffsetZ);
+        ikTarget.position = inputTransform.position +  inputTransform.TransformVector(trackingPositionOffset);
         ikTarget.rotation = inputTransform.rotation * Quaternion.Euler(trackingRotationOffset);
     }
     
@@ -40,8 +53,16 @@ public class WristMap : IMap{
 
 public class PhysicsHand : MonoBehaviour {
 
-    public WristMap wrist;
+//    public WristMap wrist;
     public FingerMap index, middle, pinky, ring, thumb;
+
+//    private void Start() {
+//        index.Init();
+//        middle.Init();
+//        pinky.Init();
+//        ring.Init();
+//        thumb.Init();
+//    }
 
     private void Update() {
         index.Map();
@@ -49,6 +70,6 @@ public class PhysicsHand : MonoBehaviour {
         pinky.Map();
         ring.Map();
         thumb.Map();
-        wrist.Map();
+//        wrist.Map();
     }
 }
