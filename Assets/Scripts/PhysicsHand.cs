7c65cafd827bcc7caf4b7d9b26d5a8905679b9de
Clayton Stamper <claystamper@gmail.com>
2020-04-22 15:54:43 -0500

    refactored controller finger controls to work individually and via animating the ik targets.

--- Assets/Scripts/PhysicsHand.cs
+++ Assets/Scripts/PhysicsHand.cs
@@ -56,9 +56,10 @@ public class PhysicsHand : MonoBehaviour {
     public void ActivateHands(bool activeHands) {
 
         handTrackingIsActive = activeHands;
-        foreach (RigBuilder.RigLayer rigLayer in GetComponent<RigBuilder>().layers) {
-            rigLayer.rig.weight = activeHands ? 1 : 0;
-        }
+        anim.SetBool("handTrackingIsActive", activeHands);
+//        foreach (RigBuilder.RigLayer rigLayer in GetComponent<RigBuilder>().layers) {
+//            rigLayer.rig.weight = activeHands ? 1 : 0;
+//        }
 
         if (handTrackingIsActive) {
             transform.localPosition = handtrackingOffsets.position;
@@ -80,8 +81,76 @@ public class PhysicsHand : MonoBehaviour {
     }
 
     private void MapToControllerInput() {
-        anim.SetFloat("OpenValue", OVRInput.Get(
-            isRightHand ? OVRInput.Axis1D.SecondaryHandTrigger : OVRInput.Axis1D.PrimaryHandTrigger));
+
+        float openThumb = 0, openFingers = 0, openIndex = 0;
+        
+        if (isRightHand) { //check right hand input
+
+            //check thumb
+            if (OVRInput.Get(OVRInput.Button.One) || OVRInput.Get(OVRInput.Button.Two)) {
+                openThumb = 0; //thumb button pressed
+            } else if (OVRInput.Get(OVRInput.Touch.One) || (OVRInput.Get(OVRInput.Touch.Two))) {
+                openThumb = 0.8f; //thumb button touched
+            } else {
+                openThumb = 1f; // thumb buttons are untouched
+            }
+            
+            //check index input
+            float indexInput = OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger);
+            //trigger is not being pressed
+            if (indexInput <= 0 ) {
+                //if touching, close a little
+                if (OVRInput.Get(OVRInput.NearTouch.SecondaryIndexTrigger))
+                    openIndex = 0.8f;
+                else
+                    openIndex = 1f;
+            }
+            else {                 //trigger is being pressed
+                openIndex = indexInput.Remap(0, 1, 0.8f, 0.0f);
+            }
+            
+            //check middle finger ("hand input") input
+            float handInput = OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger);
+            openFingers = handInput.Remap(0, 1, 1, 0f);
+
+        }
+        else { //check left hand input
+            
+            //check thumb
+            if (OVRInput.Get(OVRInput.Button.Three) || OVRInput.Get(OVRInput.Button.Four)) {
+                openThumb = 0; //thumb button pressed
+            } else if (OVRInput.Get(OVRInput.Touch.Three) || (OVRInput.Get(OVRInput.Touch.Four))) {
+                openThumb = 0.9f; //thumb button touched
+            } else {
+                openThumb = 1f; // thumb buttons are untouched
+            }
+            
+            //check index input
+            float indexInput = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger);
+            //trigger is not being pressed
+            if (indexInput <= 0 ) {
+                //if touching, close a little
+                if (OVRInput.Get(OVRInput.NearTouch.PrimaryIndexTrigger))
+                    openIndex = 0.9f;
+                else
+                    openIndex = 1f;
+            }
+            else {                 //trigger is being pressed
+                openIndex = indexInput.Remap(0, 1, 0.8f, 0.0f);
+            }
+            
+            //check middle finger ("hand input") input
+            float handInput = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger);
+            openFingers = handInput.Remap(0, 1, 1, 0f);
+            
+        }
+        
+        
+        
+        anim.SetFloat("open_thumb", openThumb);
+        anim.SetFloat("open_index", openIndex);
+        anim.SetFloat("open_fingers", openFingers);
+        
     }
     
 }
