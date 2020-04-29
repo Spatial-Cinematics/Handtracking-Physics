fa870290f0642505fed8f89296d2631aa4080a88
Clayton Stamper <claystamper@gmail.com>
2020-04-29 02:27:41 -0500

    Added functionality to lever

---
+++ Assets/Scripts/SunMover.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class SunMover : MonoBehaviour {
+
+    public Transform sun;
+
+    public Vector3 rotOffset;
+    
+    private void Update() {
+
+        sun.localRotation = transform.localRotation * quaternion.Euler(rotOffset);
+
+    }
+}
